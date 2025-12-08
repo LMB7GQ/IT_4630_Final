a63cb3a91782e8d334f178bb1fd292385ffb6f67
LMB7GQ <lmb7gq@umsystem.edu>
2025-12-08 13:05:05 -0600

    Added players, floor, movement, walls
    two players 1 moves with wasd and 2 moves arrow keys
    
    may have to go in and fix project input manager if so ill show what i did but maybe not ???
    
    also players can jump off of only floors and will slide off of walls

--- Assets/Script/Player_Movement.cs
+++ Assets/Script/Player_Movement.cs
@@ -1,18 +1,68 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Player_Movement : MonoBehaviour
+public class PlayerMovementMinimal : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("Movement Settings")]
+    public float moveSpeed = 6f;
+    public float jumpForce = 12f;
+
+    private Rigidbody rb;
+    private bool isGrounded;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        HandleMovement();
+        HandleJump();
+    }
+
+    void HandleMovement()
+    {
+        float move = 0f;
+
+        if (CompareTag("Player_1"))
+            move = Input.GetAxisRaw("Horizontal_WASD");
+        else if (CompareTag("Player_2"))
+            move = Input.GetAxisRaw("Horizontal_Arrows");
+
+        Vector3 velocity = rb.velocity;
+        velocity.x = move * moveSpeed;
+        rb.velocity = new Vector3(velocity.x, velocity.y, rb.velocity.z);
+    }
+
+    void HandleJump()
+    {
+        bool jumpPressed = false;
+
+        if (CompareTag("Player_1"))
+            jumpPressed = Input.GetKeyDown(KeyCode.W);
+        else if (CompareTag("Player_2"))
+            jumpPressed = Input.GetKeyDown(KeyCode.UpArrow);
+
+        if (jumpPressed && isGrounded)
+        {
+            rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
+        }
+    }
+
+    // Ground check via collision
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Floor"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Floor"))
+        {
+            isGrounded = false;
+        }
     }
 }
